64b5d9964aa6379efea59cad3de2bd89885aef6f
unknown <cherif@beijin.unice.fr>
2022-02-18 13:45:17 +0100

    SupportPoints struct now inherits from compositionStructure to gain from children parents relationship

--- app/include/polygon_decompoe_utils.h
+++ app/include/polygon_decompoe_utils.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "defs.h"
+#include "relationships/composition_struct.h"
 
 namespace LxGeo
 {
@@ -13,13 +14,27 @@ namespace LxGeo
 			constant_walker = 1 << 2
 		};
 
-		struct SupportPoints {
-			std::vector<size_t> polygon_indices;
-			std::vector<Boost_Point_2> support_points;
+		struct SupportPoints : public compositionStrucure<Boost_Point_2> {
+			
+			//std::vector<size_t> polygon_indices;
+			//std::vector<Boost_Point_2> support_points;
+			//size_t polygon_count;
+
+			std::vector<size_t>& polygon_indices() { return this->parents_indices; };
+			std::vector<Boost_Point_2>& support_points() { return this->children; };
+			size_t& polygon_count() { return this->parent_count; };
+
+			template<typename child_proprety, typename parent_proprety>
+			std::vector<parent_proprety> aggregate_points_to_polygon(std::vector<child_proprety>& children_properties,
+				std::function< parent_proprety(std::list<child_proprety>) > properties_aggregator) { return aggregate_children_to_parent(children_properties, properties_aggregator); }
+
+
 		};
 
 		SupportPoints decompose_polygons(std::vector<Boost_Polygon_2>& input_polygons,
 			SupportPointsStrategy decompose_strategy = SupportPointsStrategy::vertex_and_mid_point);
+		
+		
 
 	}
 }
