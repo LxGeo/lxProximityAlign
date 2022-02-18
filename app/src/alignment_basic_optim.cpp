ccbb26dc9339b3458c9af0e28d0f6c4bd6df4100
unknown <cherif@beijin.unice.fr>
2022-02-18 13:47:36 +0100

    fixing supportPoints member call && usage of parent children properties aggregator for polygon disparity calculation

--- app/src/alignment_basic_optim.cpp
+++ app/src/alignment_basic_optim.cpp
@@ -14,11 +14,11 @@ namespace LxGeo
 
 			RasterIO& ref_raster = rasters_map["proximity"];
 			// Support point generation
-			SupportPoints c_sup_pts = decompose_polygons(input_polygons);
+			SupportPoints c_sup_pts = decompose_polygons(input_polygons, SupportPointsStrategy::constant_walker);
 
 			// spatial to pixel coords
-			std::vector<PixelCoords> c_sup_pixels; c_sup_pixels.reserve(c_sup_pts.polygon_indices.size());
-			std::transform(c_sup_pts.support_points.begin(), c_sup_pts.support_points.end(),
+			std::vector<PixelCoords> c_sup_pixels; c_sup_pixels.reserve(c_sup_pts.polygon_indices().size());
+			std::transform(c_sup_pts.support_points().begin(), c_sup_pts.support_points().end(),
 				std::back_inserter(c_sup_pixels),
 				[&ref_raster](auto& pt)->auto{return ref_raster.get_pixel_coords(pt); }
 			);
@@ -36,26 +36,12 @@ namespace LxGeo
 			std::vector<SpatialCoords> support_points_disp; support_points_disp.reserve(proximity_triplets.size());
 			std::transform(proximity_triplets.begin(), proximity_triplets.end(),
 				std::back_inserter(support_points_disp),
-				[&](ProximityTriplet& a)->SpatialCoords { return { a.prox_value * sign(a.grad_x), a.prox_value * sign(a.grad_y) }; }
+				//[&](ProximityTriplet& a)->SpatialCoords { return { a.prox_value * sign(a.grad_y), a.prox_value * sign(a.grad_x) }; }
+				ptl_aggregator_function
 			);
 
 			////// aggeragte disparites by polygon
-			// compute polygon_disparities
-			std::map<size_t, std::pair<SpatialCoords, size_t>> polygons_disp_map; // key is polygon_idx & value is pair of (summed support points disparities) & (count of support points)
-			for (size_t sup_pt_idx = 0; sup_pt_idx < c_sup_pts.polygon_indices.size(); ++sup_pt_idx) {
-				size_t& polygon_idx = c_sup_pts.polygon_indices[sup_pt_idx];
-				if (polygons_disp_map.find(polygon_idx) == polygons_disp_map.end()) {
-					polygons_disp_map[polygon_idx] = std::make_pair(SpatialCoords({ 0,0 }), 0);
-				}
-				else {
-					polygons_disp_map[polygon_idx].first += support_points_disp[sup_pt_idx];
-					polygons_disp_map[polygon_idx].second++;
-				}
-			}
-			// aggregate
-			std::vector<SpatialCoords> polygon_disp_values; polygon_disp_values.reserve(polygons_disp_map.size());
-			for (size_t poly_idx = 0; poly_idx < input_polygons.size(); ++poly_idx)
-				polygon_disp_values.push_back(polygons_disp_map[poly_idx].first / double(polygons_disp_map[poly_idx].second));
+			std::vector<SpatialCoords> polygon_disp_values = c_sup_pts.aggregate_points_to_polygon<SpatialCoords, SpatialCoords>(support_points_disp, spatial_coords_median_aggregator);
 
 			//// Align geometries
 			std::vector<Boost_Polygon_2> aligned_polygon = translate_geometries(input_polygons, polygon_disp_values);
