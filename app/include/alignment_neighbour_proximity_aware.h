a7b36d8526a067da7e524d675d587ca5e421a625
unknown <cherif@beijin.unice.fr>
2022-03-01 17:33:17 +0100

    adding proximity aware method for alignment

---
+++ app/include/alignment_neighbour_proximity_aware.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "defs.h"
+#include "io_raster.h"
+
+namespace LxGeo
+{
+
+	using namespace IO_DATA;
+	namespace lxProximityAlign
+	{
+
+		std::vector<Boost_Polygon_2> alignmentNeighbourProxAware(std::map<std::string, matrix>& matrices_map, RasterIO& ref_raster, std::vector<Boost_Polygon_2>& input_polygons);
+
+	}
+}
---
+++ app/src/alignment_neighbour_proximity_aware.cpp
@@ -0,0 +1,102 @@
+#include "alignment_neighbour_proximity_aware.h"
+
+#include "defs.h"
+#include "parameters.h"
+#include "io_shapefile.h"
+#include "alignment_neighbour_optim.h"
+#include "polygon_decompoe_utils.h"
+#include "proximity_triplet.h"
+#include "affine_geometry/affine_transformer.h"
+#include "graph_weights/polygons_spatial_weights.h"
+#include "graph_weights/spatial_weights.h"
+#include "relationships/composition_struct.h"
+
+namespace LxGeo
+{
+
+	using namespace IO_DATA;
+	namespace lxProximityAlign
+	{
+
+		std::vector<Boost_Polygon_2> alignmentNeighbourProxAware(std::map<std::string, matrix>& matrices_map, RasterIO& ref_raster, std::vector<Boost_Polygon_2>& input_polygons) {
+
+
+			// Support point generation
+			SupportPoints c_sup_pts = decompose_polygons(input_polygons, SupportPointsStrategy::constant_walker);
+
+			// spatial to pixel coords
+			std::vector<PixelCoords> c_sup_pixels; c_sup_pixels.reserve(c_sup_pts.polygon_indices().size());
+			std::transform(c_sup_pts.support_points().begin(), c_sup_pts.support_points().end(),
+				std::back_inserter(c_sup_pixels),
+				[&ref_raster](auto& pt)->auto{return ref_raster.get_pixel_coords(pt); }
+			);
+
+			// proximity triplet reader creation (used to read seperate pixel values from image arrays)
+			ProximityTripletLoader PTL(matrices_map["proximity"],
+				matrices_map["grad_x"],
+				matrices_map["grad_y"]);
+
+			// Read proximity triplet
+			std::vector<ProximityTriplet> proximity_triplets; proximity_triplets.reserve(c_sup_pixels.size());
+			std::transform(c_sup_pixels.begin(), c_sup_pixels.end(), std::back_inserter(proximity_triplets), [&PTL](auto& px)->ProximityTriplet {return PTL.readTripletAt(px); });
+
+
+			// convert triplet to disparity (disparity is the proximity multiplied by the sign of the grad at X & Y axis)
+			std::vector<SpatialCoords> support_points_disp; support_points_disp.reserve(proximity_triplets.size());
+			std::transform(proximity_triplets.begin(), proximity_triplets.end(),
+				std::back_inserter(support_points_disp),
+				ptl_aggregator_function
+			);
+
+			//// translate points
+			std::vector<Boost_Point_2> translated_points = translate_geometries(c_sup_pts.support_points(), support_points_disp);
+
+			// spatial to pixel coords
+			std::vector<PixelCoords> translated_sup_pixels; translated_sup_pixels.reserve(c_sup_pixels.size());
+			std::transform(translated_points.begin(), translated_points.end(),
+				std::back_inserter(translated_sup_pixels),
+				[&ref_raster](auto& pt)->auto{return ref_raster.get_pixel_coords(pt); }
+			);
+			// Read proximity triplet
+			std::vector<ProximityTriplet> translated_proximity_triplets; translated_proximity_triplets.reserve(translated_sup_pixels.size());
+			std::transform(translated_sup_pixels.begin(), translated_sup_pixels.end(), std::back_inserter(translated_proximity_triplets), [&PTL](auto& px)->ProximityTriplet {return PTL.readTripletAt(px); });
+			std::vector<double> support_pts_weights; support_pts_weights.reserve(translated_sup_pixels.size());
+			auto before_after_prox_weight = [](double p_b, double p_a)->double { return double(1) - ((p_a - p_b) / (1 + p_a + p_b)); };
+			for (size_t c_pt_idx = 0; c_pt_idx < translated_sup_pixels.size();++c_pt_idx) 
+				support_pts_weights.push_back(before_after_prox_weight(proximity_triplets[c_pt_idx].prox_value, translated_proximity_triplets[c_pt_idx].prox_value));
+
+			////// weighted aggeragted disparites by polygon
+			std::vector<SpatialCoords> polygon_disp_values = c_sup_pts.aggregate_points_to_polygon<SpatialCoords, SpatialCoords>(support_points_disp, support_pts_weights, spatial_coords_weighted_mean_aggregator);
+
+
+			// Spatial weights
+			PolygonSpatialWeights PSW = PolygonSpatialWeights(input_polygons);
+			WeightsDistanceBandParams wdbp = { 2, false, -1, [](double x)->double { return 1.0 / (1.0 + x); } };
+			PSW.fill_distance_band_graph(wdbp);
+			PSW.run_labeling();
+
+			std::vector<LineString_with_attributes> edges_line_strings = PSW.export_edge_graph_as_LSwithAttr();
+			std::string component_shapefile_path = (boost::filesystem::path(params->temp_dir) / "components.shp").string();
+			LineStringShapfileIO edges_out_shapefile = LineStringShapfileIO(component_shapefile_path, ref_raster.spatial_refrence);
+			edges_out_shapefile.write_linestring_shapefile(edges_line_strings);
+
+			// aggregate diparites by block
+			compositionStrucure<Boost_Polygon_2> block_to_poly_relation;
+			block_to_poly_relation.parents_indices = PSW.component_labels;
+			block_to_poly_relation.parent_count = PSW.n_components;
+			std::vector<SpatialCoords> block_disp_values = block_to_poly_relation.aggregate_children_to_parent<SpatialCoords, SpatialCoords>(polygon_disp_values, spatial_coords_mean_aggregator);
+
+			for (size_t poly_idx = 0; poly_idx < polygon_disp_values.size(); ++poly_idx) {
+				size_t respective_block_idx = PSW.component_labels[poly_idx];
+				polygon_disp_values[poly_idx] = block_disp_values[respective_block_idx];
+			}
+
+			//// Align geometries
+			std::vector<Boost_Polygon_2> aligned_polygon = translate_geometries(input_polygons, polygon_disp_values);
+
+			return aligned_polygon;
+
+		}
+
+	}
+}
